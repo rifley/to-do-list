61cf058119a34ff1c6afcfbfc338e75a245c2423
Nathaniel Meyer and Riley Watts <nathaniel@>
2017-05-03 15:11:20 -0700

    Add filter by completeness with a pipe

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,13 +4,25 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
-  <ul>
-    <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
-  </ul>
+    <select (change)="onChange($event.target.value)">
+      <option value="allTasks">All Tasks</option>
+      <option value="completedTasks">Completed Tasks</option>
+      <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+    </select>
+
+
+    <ul>
+      <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+        <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+        <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+        <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>
+      </li>
+    </ul>
   `
 })
 
 export class TaskListComponent {
+  filterByCompleteness: string = "incompleteTasks";
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
 
@@ -25,6 +37,15 @@ export class TaskListComponent {
       alert("This task is not done. Better get to work!");
     }
   }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
+  }
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
+  }
+
 
   priorityColor(currentTask){
     if (currentTask.priority === 3){
