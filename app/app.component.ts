18f30624cbd1495d16a623cf809e80c8d80881e7
Nathaniel Meyer and Riley Watts <nathaniel@>
2017-05-03 14:08:03 -0700

    Finish refactoring components

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,23 +7,9 @@ import { Task } from './task.model';
     <div class="container">
       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
       <h3>{{currentFocus}}</h3>
-      <task-list></task-list>
+      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
       <hr>
-      <div>
-        <div *ngIf="selectedTask">
-          <h3>{{selectedTask.description}}</h3>
-          <p>Task Complete? {{selectedTask.done}}</p>
-          <h3>Edit Task</h3>
-          <label>Enter Task Description:</label>
-          <input [(ngModel)]="selectedTask.description">
-          <label>Enter Task Priority (1-3):</label>
-          <br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-          <button (click)="finishedEditing()">Done</button>
-        </div>
-      </div>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
     </div>
   `
 })
@@ -34,6 +20,13 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+
 
   selectedTask = null;
 
