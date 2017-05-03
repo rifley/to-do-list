4c4a5a99001caf2d73628653571916ea2ef7cf19
Keith Stedman <keithstedman@gmail.com>
2017-05-03 09:03:29 -0700

    Add conditional directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,26 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-       <div class="container">
-      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-      <h3>{{currentFocus}}</h3>
-      <ul>
-        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-      </ul>
-      <hr>
-      <div>
-       <h3>{{selectedTask.description}}</h3>
-       <p>Task Complete? {{selectedTask.done}}</p>
+<div class="container">
+  <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+  <h3>{{currentFocus}}</h3>
+  <ul>
+    <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+  </ul>
+  <hr>
+  <div>
+    <div *ngIf="selectedTask">
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task Complete? {{selectedTask.done}}</p>
       <h3>Edit Task</h3>
       <label>Enter Task Description:</label>
       <input [(ngModel)]="selectedTask.description">
-       <label>Enter Task Priority (1-3):</label>
-       <br>
-       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-       <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-      </div>
+      <label>Enter Task Priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      <button (click)="finishedEditing()">Done</button>
     </div>
+  </div>
+</div>
   `
 })
 
@@ -37,7 +40,7 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
   editTask(clickedTask){
     this.selectedTask= clickedTask;
@@ -51,6 +54,9 @@ export class AppComponent {
     }
   }
 
+  finishedEditing() {
+    this.selectedTask = null;
+  }
   priorityColor(currentTask){
     if (currentTask.priority === 3){
       return "bg-danger";
